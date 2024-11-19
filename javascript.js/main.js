8859350e429d3320c5be69c02583e964fc95bc38
yhelssy23 <yhelssyrevollo2007@cesanrafael.org>
2024-11-19 14:39:06 -0400

    first commit

---
+++ javascript.js/main.js
@@ -0,0 +1,41 @@
+let mensaje="hola mundo";
+console.log(mensaje);
+let numeroUno=2;
+let numeroDos=4;
+let numeroTres=10;
+let resultadoSuma=numeroUno+numeroDos+numeroTres;
+let resultadoResta=numeroUno-numeroDos-numeroTres;
+let resultadoDivicion=numeroUno/numeroDos/numeroTres;
+let resultadoMultiplicacion=numeroUno*numeroDos*numeroTres;
+console.log("la suma es",resultadoSuma);
+console.log("la resta es",resultadoResta);
+console.log("la divicion es",resultadoDivicion);
+console.log("la multiplicacion es",resultadoMultiplicacion);
+function sumar(){ 
+    let numeroUno=3;
+    let numeroDos=4;
+    let numeroTres=10;
+    let resultado=numeroUno+numeroDos+numeroTres
+    alert("la suma es"+resultado);
+ }
+ function restar(){ 
+    let numeroUno=3;
+    let numeroDos=4;
+    let numeroTres=10;
+    let resultado=numeroUno-numeroDos-numeroTres
+    alert("la restar es"+resultado);
+ }
+ function multiplicacion(){ 
+    let numeroUno=3;
+    let numeroDos=4;
+    let numeroTres=10;
+    let resultado=numeroUno*numeroDos*numeroTres
+    alert("la multiplicacion es"+resultado);
+ }
+ function dividir(){ 
+    let numeroUno=3;
+    let numeroDos=4;
+    let numeroTres=10;
+    let resultado=numeroUno/numeroDos/numeroTres
+    alert("la dividir es"+resultado);
+ }
